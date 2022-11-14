fe28ad3b1423248b1e4c8ea1e625f1adcbdfa2aa
guokun <1660604555@qq.com>
2022-11-14 17:04:58 +0800

    react环境搭建

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h3>app组件</h3>
+           
+            
+            </div>
+        );
+    }
+}
+
+export default App;
